755c6d6f2680f20bb68f8aab7c86c299f848d940
Clipertrucado <145342712+Clipertrucado@users.noreply.github.com>
2023-11-06 14:27:30 +0100

    Add files via upload

--- Ejercicio5(Obligatorio)/Servicios/OperacionImplementacion.cs
+++ Ejercicio5(Obligatorio)/Servicios/OperacionImplementacion.cs
@@ -17,6 +17,7 @@ namespace Ejercicio5.Servicios
                 // si los son de 4 no lo son de 100 es bisiesto
                 if (anyo % 100 != 0)
                 {
+                    Console.WriteLine("Este año es bisiesto.");
                     //se llama al metodo bisiesto
                     bisiesto(mes);
 
@@ -24,6 +25,7 @@ namespace Ejercicio5.Servicios
                 //si lo es de 4 pero tambien de 100 no lo sera de 400 es bisiesto
                 else if (anyo % 100 == 0 && anyo % 400 == 0)
                 {
+                    Console.WriteLine("Este año es bisiesto.");
                     //se llama al metodo bisiesto
                     bisiesto(mes);
 
@@ -31,7 +33,7 @@ namespace Ejercicio5.Servicios
                 //si lo es de 4, 100 y 400 no sera bisiesto
                 else
                 {
-
+                    Console.WriteLine("Este año no es bisiesto.");
                     //se llama al metodo noBisiesto
                     noBisiesto(mes);
 
@@ -41,6 +43,7 @@ namespace Ejercicio5.Servicios
             //si no es dividsible entre 4 no es visiesto 
             else
             {
+                Console.WriteLine("Este año no es bisiesto.");
                 //se llama al metodo noBisiesto
                 noBisiesto(mes);
 
@@ -58,30 +61,20 @@ namespace Ejercicio5.Servicios
             //este if compara la variable que se le a pasado "mes" con el mes numerico de aquellos que tienen 31 dias
             if (mes == 1 | mes == 3 | mes == 5 | mes == 7 | mes == 8 | mes == 10 | mes == 12)
             {
-                Console.WriteLine("Este año no es bisiesto.");
                 Console.WriteLine("Este mes cuenta con 31.");
-
-
             }
             //este if compara la variable que se le a pasado "mes" con el mes que se ve afectado si el anuy es o no bisiesto
             //que de forma numeria es "2" y como es le caso Bisiesto este mes tiene 28 dias
             else if (mes == 2)
-            {
-                //28
-                Console.WriteLine("Este año no es bisiesto.");
+            {  
                 Console.WriteLine("Este mes cuenta con 28.");
             }
             //como solo hay tres tipos de meses; de 31 dias, que ya se ha comparado, de 28/29 dias, que ya se ha comparado y los meses de 30 dias
             //al no ser ni del primer ni segundo tipo tinen que serlo del tercero, entonces todos lo que entren aqui tendran 30 dias
             else
-            {
-                //30
-                Console.WriteLine("Este año no es bisiesto.");
+            {        
                 Console.WriteLine("Este mes cuenta con 30.");
             }
-
-            
-
         }
 
         /// <summary>
@@ -94,27 +87,19 @@ namespace Ejercicio5.Servicios
             //este if compara la variable que se le a pasado "mes" con el mes numerico de aquellos que tienen 31 dias
             if (mes == 1 | mes == 3 | mes == 5 | mes == 7 | mes == 8 | mes == 10 | mes == 12)
             {
-            //31
-                Console.WriteLine("Este año es bisiesto.");
                 Console.WriteLine("Este mes cuenta con 31.");
-
             }
             //este if compara la variable que se le a pasado "mes" con el mes que se ve afectado si el anuy es o no bisiesto
             //que de forma numeria es "2" y como es le caso Bisiesto este mes tiene 28 dias
             else if (mes == 2)
             {
-            //28
-                Console.WriteLine("Este año es bisiesto.");
                 Console.WriteLine("Este mes cuenta con 29.");
             }
             //como solo hay tres tipos de meses; de 31 dias, que ya se ha comparado, de 28/29 dias, que ya se ha comparado y los meses de 30 dias
             //al no ser ni del primer ni segundo tipo tinen que serlo del tercero, entonces todos lo que entren aqui tendran 30 dias
             else
             {
-            //30
-                Console.WriteLine("Este año es bisiesto.");
                 Console.WriteLine("Este mes cuenta con 30.");
-
             }
         }
 
